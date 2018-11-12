671b80c823c145c41a2a46d1dbb65ff07e2a58c0
Adeyefa Toba <sainttobs@gmail.com>
2018-11-12 22:33:17 +0100

    Practicing data-binding

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 new Vue({
 	el: "#vue-app",
 	data:{
-		name : "Adeyefa"
+		name : "Adeyefa Oluwatoba Adegoke",
+		job : "Software Engineer",
+		stack : "NodeJS, MongoDB, Express, VueJS, Java",
+		website: "http://www.9jatechguru.com.ng",
+		websiteTag: '<a href="http://www.9jatechguru.com.ng">9jatechguru Blog </a>'
+	},
+	methods: {
+		greet : function (time) {
+			return "Good " + time +" " + this.name;
+			// body...
+		},
 	}
 });
