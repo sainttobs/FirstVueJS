9cfb42c5c902f35520fda04001eab01d09438a75
Adeyefa Toba <sainttobs@gmail.com>
2018-11-12 23:20:13 +0100

    Events

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ new Vue({
 		job : "Software Engineer",
 		stack : "NodeJS, MongoDB, Express, VueJS, Java",
 		website: "http://www.9jatechguru.com.ng",
-		websiteTag: '<a href="http://www.9jatechguru.com.ng">9jatechguru Blog </a>'
+		websiteTag: '<a href="http://www.9jatechguru.com.ng">9jatechguru Blog </a>',
+		age: 19,
+		x: 0,
+		y: 0
 	},
 	methods: {
 		greet : function (time) {
 			return "Good " + time +" " + this.name;
 			// body...
 		},
+		add : function(inc){
+			this.age += inc;
+		},
+		subtract : function(dec){
+			this.age -= dec;
+		},
+		updateXY : function(event){
+			this.x = event.offsetX;
+			this.y = event.offsetY;
+		}
 	}
 });
